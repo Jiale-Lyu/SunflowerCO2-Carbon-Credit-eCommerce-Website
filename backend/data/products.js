3bbefe3d27f6bde6d555903a6b9934e7d333e5df
Jiale-Lyu <lyu.jial@northeastern.edu>
2024-07-08 14:01:46 -0400

    leave only one product

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,17 +1,5 @@
 const products = [
   {
-    name: 'Carbon Offset Sticker',
-    image: '/images/sticker.jpg',
-    description:
-      'Embrace sustainability on the go with the EcoDrive Carbon Offset Sticker. Designed for eco-conscious drivers, this durable sticker showcases your commitment to reducing carbon footprints with every mile.  Made with weather-resistant materials, it is perfect for placing on your car, bike, or laptop.',
-    brand: 'CarbonFar',
-    category: 'Sticker',
-    price: 9.99,
-    countInStock: 0,
-    rating: 4.5,
-    numReviews: 12,
-  },
-  {
     name: 'Carbon Offset Card',
     image: '/images/card.jpg',
     description:
@@ -19,19 +7,7 @@ const products = [
     brand: 'CarbonFar',
     category: 'Card',
     price: 20,
-    countInStock: 10,
-    rating: 4.5,
-    numReviews: 12,
-  },
-  {
-    name: 'Virtual Carbon Certificate',
-    image: '/images/certificate.jpg',
-    description:
-      'Step into a brighter future with our Virtual Carbon Certificate. Every purchase represents a pledge towards sustainability and supports projects dedicated to reducing carbon emissions. Its digital format makes it an eco-friendly choice, perfect for those who wish to make a difference without leaving a physical footprint.',
-    brand: 'CarbonFar',
-    category: 'Certificate',
-    price: 19.99,
-    countInStock: 10,
+    countInStock: 20,
     rating: 4.5,
     numReviews: 12,
   },
