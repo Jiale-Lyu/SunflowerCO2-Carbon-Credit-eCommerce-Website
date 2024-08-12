ffe74ff7a7208a93a7c3a8624413d4e17e3bd5e0
Jiale-Lyu <lyu.jial@northeastern.edu>
2024-08-12 10:11:57 -0400

    update learn page

--- frontend/src/screens/AboutScreen.jsx
+++ frontend/src/screens/AboutScreen.jsx
@@ -7,7 +7,7 @@ const AboutScreen = () => {
       <h1>About Us</h1>
       <br />
 
-      <h3>Your Simple Path to Environmental Impact</h3>
+      {/* <h3>Your Simple Path to Environmental Impact</h3> */}
       <p>
         At Sunflower.com, we're dedicated to making it easy for you to
         contribute to a greener planet. We've partnered with Verra, a leading
@@ -37,40 +37,77 @@ const AboutScreen = () => {
         this large contract into 1,000 individual Carbon Offset Cards. Each card
         represents one ton of carbon credits from our Verra-backed contract. By
         purchasing a Carbon Offset Card, you are directly supporting the
-        reduction of one ton of carbon emissions. Here's why our Carbon Offset
-        Cards are a meaningful and impactful choice:
+        reduction of one ton of carbon emissions.
       </p>
-      <Row>
-        <Col md={7}>
-          <ol>
-            <li>
-              Direct Impact: Each Carbon Offset Card is backed by a portion of
-              our 1,000-ton contract, ensuring that your purchase has a real and
-              measurable impact on the environment.
-            </li>
-            <li>
-              Accessibility: Traditionally, carbon credits are sold in large
-              quantities, making it difficult for individuals to contribute. Our
-              cards break down this barrier, allowing you to participate in
-              carbon offsetting in a manageable way.
-            </li>
-            <li>
-              Permanent Commitment: Once we've purchased the carbon credit
-              contract, we do not sell it again. This means that your investment
-              in a Carbon Offset Card is secure and dedicated solely to
-              offsetting carbon emissions.
-            </li>
-            <li>
-              Environmental Stewardship: By choosing our Carbon Offset Card, you
-              are taking a tangible step toward mitigating climate change and
-              supporting a sustainable future.
-            </li>
-          </ol>
-        </Col>
-        <Col md={5}>
-          <img src='../images/aboutimg.jpg' alt='env_image' />
-        </Col>
-      </Row>
+      <p>
+        Here's why our Carbon Offset Cards are a meaningful and impactful
+        choice:
+      </p>
+      <div className='about-reason'>
+        <Row>
+          <Col md={6}>
+            <div className='about-text'>
+              <h3>Direct Impact</h3>
+              <p>
+                Each Carbon Offset Card is backed by a portion of our 1,000-ton
+                contract, ensuring that your purchase has a real and measurable
+                impact on the environment.
+              </p>
+            </div>
+          </Col>
+          <Col md={6}>
+            <img src='../images/about1.jpg' alt='env_image' />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <img src='../images/about2.jpg' alt='env_image' />
+          </Col>
+          <Col>
+            <div className='about-text'>
+              <h4>Accessibility</h4>
+              <p>
+                Traditionally, carbon credits are sold in large quantities,
+                making it difficult for individuals to contribute. Our cards
+                break down this barrier, allowing you to participate in carbon
+                offsetting in a manageable way.
+              </p>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <div className='about-text'>
+              <h4>Permanent Commitment</h4>
+              <p>
+                Once we've purchased the carbon credit contract, we do not sell
+                it again. This means that your investment in a Carbon Offset
+                Card is secure and dedicated solely to offsetting carbon
+                emissions.
+              </p>
+            </div>
+          </Col>
+          <Col>
+            <img src='../images/about3.jpg' alt='env_image' />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <img src='../images/about4.jpg' alt='env_image' />
+          </Col>
+          <Col>
+            <div className='about-text'>
+              <h4>Environmental Stewardship</h4>
+              <p>
+                By choosing our Carbon Offset Card, you are taking a tangible
+                step toward mitigating climate change and supporting a
+                sustainable future.
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </div>
+
       <p>
         Every card you purchase helps reduce the carbon footprint and promotes
         cleaner air and a healthier planet. Join us in making a difference—one
